8857773513829dd9428545d95b3a510edc4aebbf
Philip O'Toole <philip.otoole@yahoo.com>
2022-08-20 14:56:55 -0400

    Support printing progress every N

--- indexer.py
+++ indexer.py
@@ -6,7 +6,7 @@ import time
 # Visit https://github.com/rqlite/pyrqlite to install Python client library.
 import pyrqlite.dbapi2 as dbapi2
 
-def index_logs(file, host, port):
+def index_logs(file, host, port, progress):
 	# Connect to the rqlite database.
 	connection = dbapi2.connect(host=host,port=port)
 
@@ -27,6 +27,8 @@ def index_logs(file, host, port):
 	for entry in logs:
 		cursor.execute(sql, (entry.strip(),), queue=True)
 		n+=1
+		if n % progress == 0:
+			print ("%d logs written, %d written per second" %  (n, n/(time.time() - start)))
 
 	duration = time.time() - start
 	print("%d total logs indexed in %.2f seconds, %d indexed per second" % (n, duration, n/duration))
@@ -36,6 +38,7 @@ if __name__ == "__main__":
 	parser.add_argument('file', metavar='FILE', type=str, help='path to log file')
 	parser.add_argument('--host', type=str, default='localhost', help='rqlite host')
 	parser.add_argument('--port', type=int, default=4001, help='rqlite port')
+	parser.add_argument('--progress', metavar='N', type=int, default=4096, help='print progress every N logs')
 	args = parser.parse_args()
 
-	index_logs(args.file, args.host, args.port)
+	index_logs(args.file, args.host, args.port, args.progress)
