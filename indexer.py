48d45bc8866e9e89ef3c44cc18a3e495d5f728c4
Philip O'Toole <philip.otoole@yahoo.com>
2022-08-23 09:42:42 -0400

    Support limit indexed number

--- indexer.py
+++ indexer.py
@@ -6,7 +6,7 @@ import time
 # Visit https://github.com/rqlite/pyrqlite to install Python client library.
 import pyrqlite.dbapi2 as dbapi2
 
-def index_logs(file, host, port, progress):
+def index_logs(file, host, port, progress, number):
 	# Connect to the rqlite database.
 	connection = dbapi2.connect(host=host,port=port)
 
@@ -29,16 +29,21 @@ def index_logs(file, host, port, progress):
 		n+=1
 		if n % progress == 0:
 			print ("%d logs written, %d written per second" %  (n, n/(time.time() - start)))
+		if n is not None and n == number:
+			break
 
 	duration = time.time() - start
 	print("%d total logs indexed in %.2f seconds, %d indexed per second" % (n, duration, n/duration))
+
+	logs.close()
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Index log data using rqlite")
 	parser.add_argument('file', metavar='FILE', type=str, help='path to log file')
 	parser.add_argument('--host', type=str, default='localhost', help='rqlite host')
 	parser.add_argument('--port', type=int, default=4001, help='rqlite port')
-	parser.add_argument('--progress', metavar='N', type=int, default=4096, help='print progress every N logs')
+	parser.add_argument('--progress', metavar='P', type=int, default=5000, help='print progress every P logs')
+	parser.add_argument('--number', metavar='N', type=int, default=None, help='number of entries to index')
 	args = parser.parse_args()
 
-	index_logs(args.file, args.host, args.port, args.progress)
+	index_logs(args.file, args.host, args.port, args.progress, args.number)
